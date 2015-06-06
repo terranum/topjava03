4d64baf19462e3f475f69ab7f481638d4cda7781
javawebinar@yandex.ru <github13>
2015-06-06 21:08:15 +0300

    Lambda samples

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,14 +1,30 @@
 package ru.javawebinar.topjava;
 
+import java.util.function.Consumer;
+
 /**
  * User: gkislin
  * Date: 15.01.2015
  *
  * @link http://javawebinar.ru/topjava/
- * @link http://caloriesmng.herokuapp.com/
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava!");
+        execute(() -> {
+            System.out.println("Hello Topjava!");
+        });
+        consume(System.out::println, "Hello Topjava!");
+    }
+
+    private static void execute(Runnable runnable) {
+        System.out.println("Start runner");
+        runnable.run();
+        System.out.println("End runner");
+    }
+
+    private static <String> void consume(Consumer<String> consumer, String out) {
+        System.out.println("Start consume");
+        consumer.accept(out);
+        System.out.println("End consume");
     }
 }
