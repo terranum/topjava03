f2431212720bdbcc3a3e35048e641fa5a135c934
Grigory Kislin <javawebinar@yandex.ru>
2015-07-11 15:28:48 +0300

    6_3_spring_web_init

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,7 +1,11 @@
 package ru.javawebinar.topjava.web;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.javawebinar.topjava.LoggerWrapper;
+import ru.javawebinar.topjava.service.UserService;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,11 +18,20 @@ import java.io.IOException;
  */
 public class UserServlet extends HttpServlet {
     private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+    private WebApplicationContext wac;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        wac = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("redirect to userList");
+        UserService userService = wac.getBean(UserService.class);
+        request.setAttribute("userList", userService.getAll());
 
-//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
-        response.sendRedirect("userList.jsp");
+//        response.sendRedirect("userList.jsp");
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
     }
 }
