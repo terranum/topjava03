94730eebb6d4308d13bdd21c81a81086973798a6
Grigory Kislin <javawebinar@yandex.ru>
2015-07-13 05:42:31 +0300

    6_6_spring_webmvc

---
+++ src/main/java/ru/javawebinar/topjava/web/user/UserController.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.javawebinar.topjava.service.UserService;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+@Controller
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String userList(Model model) {
+        model.addAttribute("userList", service.getAll());
+        return "userList";
+    }
+}
