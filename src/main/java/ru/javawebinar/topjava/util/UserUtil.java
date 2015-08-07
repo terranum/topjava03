49902428582efc0c7a0c183b597acd6e87b4518c
Grigory Kislin <javawebinar@yandex.ru>
2015-08-07 11:38:12 +0300

    9_3_binding

---
+++ src/main/java/ru/javawebinar/topjava/util/UserUtil.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.UserTo;
+
+/**
+ * GKislin
+ */
+public class UserUtil {
+    public static User createFromTo(UserTo newUser) {
+        return new User(null, newUser.getName(), newUser.getEmail().toLowerCase(), newUser.getPassword(), Role.ROLE_USER);
+    }
+}
